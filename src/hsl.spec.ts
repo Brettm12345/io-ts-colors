657b1f53b06752f456280f07c61a2504fd09eb1b
brettm12345 <brettmandler@gmail.com>
2020-04-15 12:14:51 -0400

    Cleanup tests

--- src/hsl.spec.ts
+++ src/hsl.spec.ts
@@ -2,14 +2,12 @@ import {Parsed} from './util'
 import {RGB, rgb} from './rgb'
 import {HSLFromRGB, hsl, HSL} from './hsl'
 import {either} from 'fp-ts/lib/Either'
-
-const colors: Array<[Parsed<RGB>, Parsed<HSL>]> = [
+;[
   [rgb(255, 0, 0), hsl(0, 100, 50)],
   [rgb(25, 26, 42), hsl(236, 25.4, 13.1)],
   [rgb(255, 255, 255), hsl(0, 0, 1)],
-]
-colors.forEach(([a, b], i) =>
+].forEach(([rgb, hsl]: [Parsed<RGB>, Parsed<HSL>], i) =>
   test(`HSL From RGB ${i}`, () => {
-    expect(either.chain(a, HSLFromRGB.decode)).toEqual(b)
+    expect(either.chain(rgb, HSLFromRGB.decode)).toEqual(hsl)
   })
 )
